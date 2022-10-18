0bcf98b4b4c4919d98be95454f0dbf962ba4594b
Vasyl <vgostiahvvasila@gmail.com>
2022-10-18 10:21:35 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-})
-
-
 ;( function ( document, window, index ) {
 	var inputs = document.querySelectorAll( '.inputfile' );
 	Array.prototype.forEach.call( inputs, function( input )
